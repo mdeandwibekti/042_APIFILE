a2626ffb23f63a30dd51a2c54664a7f968834483
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-24 12:49:51 +0700

    ememperbaiki code pada index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', apiRoutes);
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
